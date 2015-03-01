3669e8f8da951d09393bb89a114774cae7a32890
jonnew <jpnewman@mit.edu>
2015-03-01 18:22:30 -0500

    Started on problem 2.9.1, 2-bit greater than circuit.

--- ch2/greater_than/greater_than_2bit.vhd
+++ ch2/greater_than/greater_than_2bit.vhd
@@ -14,11 +14,11 @@ architecture behav of gt_2b is
 	signal p: std_logic_vector(5 downto 0); -- Internal signals
 
 begin
-	-- sum of two products produces output
+	-- sum of 6 products produces output
 	output <= p(0) or p(1) or p(2) or p(3) or p(4) or p(5);
 	
 	-- define product terms
-	p(0) <= (not a(1)) and a(0) and (not b(1)) and b(0);
+	p(0) <= (not a(1)) and a(0) and (not b(1)) and (not b(0));
 	p(1) <= a(1) and (not a(0)) and (not b(1)) and (not b(0));
 	p(2) <= a(1) and a(0) and (not b(1)) and (not b(0));
 	p(3) <= a(1) and (not a(0)) and (not b(1)) and b(0);
---
+++ n01_comparitor/gt_2b_wb.vhd
@@ -0,0 +1,99 @@
+library ieee;
+use ieee.std_logic_1164.ALL;
+ 
+
+-- Testbench for the 2-bit greater than entity
+entity gt_2b is
+end gt_2b;
+ 
+architecture behavior of gt_2b is 
+    
+    -- Component Declaration for the Unit Under Test (UUT)
+    
+    component gt_2b
+    port(
+         a : IN  std_logic_vector(1 downto 0);
+         b : IN  std_logic_vector(1 downto 0);
+         output : OUT  std_logic
+        );
+    end component;
+     
+    
+    --Inputs
+    signal a : std_logic_vector(1 downto 0) := (others => '0');
+    signal b : std_logic_vector(1 downto 0) := (others => '0');
+    
+     --Outputs
+    signal output : std_logic;
+     
+    begin
+    
+        -- Instantiate the Unit Under Test (UUT)
+        uut: gt_2b port map (
+               a => a,
+               b => b,
+               output => output
+             );
+    process
+    begin
+        
+        -- Test vector 
+        a <= "00";
+        b <= "01";
+        wait for 200 ns;
+        -- Test vector 
+        a <= "00";
+        b <= "10";
+        wait for 200 ns;
+        -- Test vector 
+        a <= "00";
+        b <= "11";
+        wait for 200 ns;
+        -- Test vector 
+        a <= "01";
+        b <= "00";
+        wait for 200 ns;
+        -- Test vector 
+        a <= "01";
+        b <= "01";
+        wait for 200 ns;
+        -- Test vector
+        a <= "01";
+        b <= "10";
+        wait for 200 ns;
+        -- Test vector
+        a <= "01";
+        b <= "11";
+        wait for 200 ns;
+        -- Test vector
+        a <= "10";
+        b <= "00";
+        wait for 200 ns;
+        -- Test vector 
+        a <= "11";
+        b <= "11";
+        -- Test vector
+        wait for 200 ns;
+        a <= "10";
+        b <= "00";
+        wait for 200 ns;
+         -- Test vector 
+        a <= "01";
+        b <= "00";
+        wait for 200 ns;
+         -- Test vector
+        a <= "11";
+        b <= "10";
+        wait for 200 ns;
+         -- Test vector 
+        a <= "10";
+        b <= "11";
+        wait for 200 ns;
+ 
+        assert false
+            report "Simulation complete."
+            severity failure;
+    end process;
+    
+end behavior;
+
